9a0f7a729f1d1b60a0fdd0a84e30b2947e8d75e7
맹주영 <mjy9088@naver.com>
2019-07-15 23:08:19 +0900

    지출/수입 입력 기능

--- app/src/main/java/kleita/magenta/MainActivity.java
+++ app/src/main/java/kleita/magenta/MainActivity.java
@@ -4,10 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private final int[] IDS = {
+            R.id.btn1, R.id.btn2, R.id.btn3,
+            R.id.btn4, R.id.btn5, R.id.btn6,
+            R.id.btn7, R.id.btn8, R.id.btn9,
+            R.id.btn_b, R.id.btn0, R.id.btn_n
+    };
+
+    private int value;
+    private TextView price;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -20,5 +32,57 @@ public class MainActivity extends AppCompatActivity
         OnLayoutChangeListener listener = new OnLayoutChangeListener(((LinearLayout) findViewById(R.id.above)));
         listener.ratio = 40f/111f;
         findViewById(R.id.background).addOnLayoutChangeListener(listener);
+        price = findViewById(R.id.price);
+        for(int id : IDS)
+        {
+            findViewById(id).setOnClickListener(this);
+        }
+    }
+
+    private void setValue(int value)
+    {
+        this.value = value;
+        price.setText(String.valueOf(value));
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.btn0:
+                setValue(value * 10);
+                break;
+            case R.id.btn1:
+                setValue(value * 10 + 1);
+                break;
+            case R.id.btn2:
+                setValue(value * 10 + 2);
+                break;
+            case R.id.btn3:
+                setValue(value * 10 + 3);
+                break;
+            case R.id.btn4:
+                setValue(value * 10 + 4);
+                break;
+            case R.id.btn5:
+                setValue(value * 10 + 5);
+                break;
+            case R.id.btn6:
+                setValue(value * 10 + 6);
+                break;
+            case R.id.btn7:
+                setValue(value * 10 + 7);
+                break;
+            case R.id.btn8:
+                setValue(value * 10 + 8);
+                break;
+            case R.id.btn9:
+                setValue(value * 10 + 9);
+                break;
+            case R.id.btn_b:
+                setValue(value / 10);
+                break;
+        }
     }
 }
