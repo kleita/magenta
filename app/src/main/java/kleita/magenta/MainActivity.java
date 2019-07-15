e441224a8e87dffbb2e0dfe980a684ad5facead4
맹주영 <mjy9088@naver.com>
2019-07-15 22:51:20 +0900

    OnChangeListener 지출/수입 입력 화면에도 적용

--- app/src/main/java/kleita/magenta/MainActivity.java
+++ app/src/main/java/kleita/magenta/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,5 +17,6 @@ public class MainActivity extends AppCompatActivity
         ViewPager viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(adapter);
+        findViewById(R.id.background).addOnLayoutChangeListener(new OnChangeListener(((LinearLayout) findViewById(R.id.above))));
     }
 }
